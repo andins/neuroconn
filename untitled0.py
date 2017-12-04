d8c020181d31f41496da162dc3f40771e9b938ea
andins <a.insabato@gmail.com>
2017-12-04 17:00:59 -0500

    some classification functions

--- untitled0.py
+++ untitled0.py
@@ -8,15 +8,20 @@ Created on Mon Dec  4 08:30:06 2017
 
 import numpy as np
 from MOU_estimation import MOU_Lyapunov
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
 
 
 class subject:
     """
     Basic class for subject
     """
-    def __init__(self, SC=None):
+    def __init__(self, ID, SC=None):
         self.sessions = list()
         self.SC = SC
+        self.ID = ID
 
 
 class session:
@@ -51,7 +56,7 @@ class test_retest_dataset:
         self.n_time_samples = np.shape(BOLD_ts)[3]
         self.subjects = list()
         for sb in range(self.n_subjects):
-            self.subjects.append(subject())
+            self.subjects.append(subject(sb))
             for ss in range(self.n_sessions):
                 if conditions is None:  # assign all 1s if no condition
                     cond = 1
@@ -64,7 +69,7 @@ class test_retest_dataset:
                     raise ValueError("conditions has to be a list",
                                      "or numpy array.")
                 # TODO: add same tests for time
-                self.subjects[sb].sessions.append(session(BOLD_ts[sb, ss, :, :], condition=cond), time=time)
+                self.subjects[sb].sessions.append(session(BOLD_ts[sb, ss, :, :], condition=cond, time=time))
 
     def estimate_EC(self, subjects, sessions, norm_fc=None):
         # TODO: modify to produce fitting graphics in a separate folder
@@ -112,25 +117,70 @@ class test_retest_dataset:
     def minimalBest_VS_minimalWorst(self):
         return 0
 
+    def make_data_matrix(self, subjects=None, sessions=None, C='FC'):
+        # move to utils
+        """
+        Builds a data matrix [n_samples, n_features] by concatenating
+        elements of connectivity matrix C and stacking them over rows
+        for each subject and session specified in subjects and sessions.
+        """
+        if subjects is None:
+            subjects = range(self.n_subjects)
+        if sessions is None:
+            sessions = range(self.n_sessions)
+        if C is 'FC':
+            idxs = np.triu_indices(self.n_ROIs)
+            X = np.zeros([len(subjects)*len(sessions), len(idxs[0])])
+            i = 0
+            for sb in subjects:
+                for ss in sessions:
+                    X[i, :] = self.subjects[sb].sessions[ss].FC[idxs]
+                    i += 1
+        # TODO implement test for EC
+        else:
+            raise ValueError("C has to be either FC or EC.")
+        return X
 
-def split_train_test_same(X, y, train_size=0.8):
-    """
-    Split data in train and test keeping the approximate proportion of
-    samples for each class
-    """
+    def make_target_subjects(self, subjects=None, sessions=None):
+        # move to utils
+        """
+        Builds a target vector y with subjects ID to classify subjects.
+        """
+        if subjects is None:
+            subjects = range(self.n_subjects)
+        if sessions is None:
+            sessions = range(self.n_sessions)
+        y = np.zeros([len(subjects)*len(sessions)])
+        i = 0
+        for sb in subjects:
+                for ss in sessions:
+                    y[i] = self.subjects[sb].ID
+                    i += 1
+        return y
+
+
+def crossvalidate_clf(X, y, train_size, repetitions=10):
     # TODO: move to utils
-    train_idx = 0
-    test_idx = 0
-    return train_idx, test_idx
-
-
-def crossvalidate_clf(X, y):
-    # TODO: move to utils
-    # for repetitions
-        # split_train_test_same()
+    clf = LogisticRegression(C=10000, penalty='l2',
+                             multi_class='multinomial',
+                             solver='lbfgs')
+    pipe = Pipeline([('pca', PCA()),
+                     ('clf', clf)])
+    scores = np.zeros([repetitions])
+    sss = StratifiedShuffleSplit(n_splits=repetitions, test_size=None,
+                                 train_size=train_size, random_state=0)
+    r = 0  # repetitions index
+    for train_idx, test_idx in sss.split(X, y):
+        data_train = X[train_idx, :]
+        y_train = y[train_idx]
+        data_test = X[test_idx, :]
+        y_test = y[test_idx]
         # fit clf on train
+        pipe.fit(data_train, y_train)
         # predict on test
-    return 0
+        scores[r] = pipe.score(data_test, y_test)
+        r += 1
+    return scores
 
 
 def calc_mean_FC():
@@ -140,3 +190,7 @@ def calc_mean_FC():
 
 ts_emp = np.load('/home/andrea/Work/matt_movie_scripts/EC_estimation/rest_movie_ts.npy')
 movie = test_retest_dataset(ts_emp, conditions=[0, 0, 1, 1])
+movie.estimate_FC()
+X = movie.make_data_matrix()
+y = movie.make_target_subjects()
+s = crossvalidate_clf(X, y, train_size=22)
